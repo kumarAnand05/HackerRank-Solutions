2324bb9237ba98e9b397005f91ef7dd2d7733493
Anand Kumar <111251492+kumarAnand05@users.noreply.github.com>
2023-09-24 16:01:27 +0530

    Create  Java Stdin and Stdout I.java
    Most HackerRank challenges require you to read input from stdin (standard input) and write output to stdout (standard output).
    
    One popular way to read input from stdin is by using the Scanner class and specifying the Input Stream as System.in. For example:
    
    Scanner scanner = new Scanner(System.in);
    String myString = scanner.next();
    int myInt = scanner.nextInt();
    scanner.close();
    
    System.out.println("myString is: " + myString);
    System.out.println("myInt is: " + myInt);
    The code above creates a Scanner object named  and uses it to read a String and an int. It then closes the Scanner object because there is no more input to read, and prints to stdout using System.out.println(String). So, if our input is:
    
    Hi 5
    Our code will print:
    
    myString is: Hi
    myInt is: 5
    Alternatively, you can use the BufferedReader class.
    
    Task
    In this challenge, you must read  integers from stdin and then print them to stdout. Each integer must be printed on a new line. To make the problem a little easier, a portion of the code is provided for you in the editor below.
    
    Input Format
    
    There are  lines of input, and each line contains a single integer.
    
    Sample Input
    
    42
    100
    125
    Sample Output
    
    42
    100
    125

---
+++ Java Stdin and Stdout I.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+import java.util.*;
+
+public class Solution{
+    public static void main(String[] args){
+        int a,b,c;
+        Scanner s=new Scanner(System.in);
+        a= s.nextInt();
+        b= s.nextInt();
+        c= s.nextInt();
+        s.close();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.print(c);
+    }
+}
