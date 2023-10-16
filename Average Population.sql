ce879809522eb955549793e6a57dbb487fec16d0
Anand Kumar <111251492+kumarAnand05@users.noreply.github.com>
2023-10-16 11:41:34 +0530

    Average Population Solution.sql

--- Average Population.sql
+++ Average Population.sql
@@ -1,1 +1,14 @@
+/*
+Question:
+
+Query the average population for all cities in CITY, rounded down to the nearest integer.
+
+Input Format
+
+The CITY table is described as follows: CITY.jpg
+
+*/
+
+--Solution:
+
 select floor(avg(population)) from city
