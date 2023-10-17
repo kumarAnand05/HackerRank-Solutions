0dadee3801ba5501a1e00d1777703b07bf4c45f4
Anand Kumar <111251492+kumarAnand05@users.noreply.github.com>
2023-10-17 10:59:37 +0530

    Input() Solution.py

--- Input().py
+++ Input().py
@@ -1,3 +1,54 @@
+"""
+Question:
+
+This challenge is only forPython 2.
+
+input()
+In Python 2, the expression input() is equivalent to eval(raw _input(prompt)).
+
+Code
+
+>>> input()  
+1+2
+3
+>>> company = 'HackerRank'
+>>> website = 'www.hackerrank.com'
+>>> input()
+'The company name: '+company+' and website: '+website
+'The company name: HackerRank and website: www.hackerrank.com'
+Task
+
+You are given a polynomial  of a single indeterminate (or variable), .
+You are also given the values of  and . Your task is to verify if .
+
+Constraints
+All coefficients of polynomial  are integers.
+ and  are also integers.
+
+Input Format
+
+The first line contains the space separated values of  and .
+The second line contains the polynomial .
+
+Output Format
+
+Print True if . Otherwise, print False.
+
+Sample Input
+
+1 4
+x**3 + x**2 + x + 1
+Sample Output
+
+True
+Explanation
+
+
+Hence, the output is True.
+"""
+
+# Solution:
+
 x,k = list(map(int, input().split()))
 p = input()
 print(eval(''.join(map(str,[x if _ == 'x' else _ for _ in p])))==k)
