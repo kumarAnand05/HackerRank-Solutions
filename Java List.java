9a8151b38ced1adc1bf7fef6c216d5beb13b9143
Anand Kumar <111251492+kumarAnand05@users.noreply.github.com>
2023-10-18 10:45:45 +0530

    Java List Solution.java

---
+++ Java List.java
@@ -0,0 +1,97 @@
+/*
+Question:
+
+For this problem, we have  types of queries you can perform on a List:
+
+Insert  at index :
+
+Insert
+x y
+Delete the element at index :
+
+Delete
+x
+Given a list, , of  integers, perform  queries on the list. Once all queries are completed, print the modified list as a single line of space-separated integers.
+
+Input Format
+
+The first line contains an integer,  (the initial number of elements in ).
+The second line contains  space-separated integers describing .
+The third line contains an integer,  (the number of queries).
+The  subsequent lines describe the queries, and each query is described over two lines:
+
+If the first line of a query contains the String Insert, then the second line contains two space separated integers , and the value  must be inserted into  at index .
+If the first line of a query contains the String Delete, then the second line contains index , whose element must be deleted from .
+Constraints
+
+
+
+Each element in is a 32-bit integer.
+Output Format
+
+Print the updated list  as a single line of space-separated integers.
+
+Sample Input
+
+5
+12 0 1 78 12
+2
+Insert
+5 23
+Delete
+0
+Sample Output
+
+0 1 78 12 23
+Explanation
+
+
+ Insert 23 at index .
+
+ Delete the element at index .
+
+Having performed all  queries, we print  as a single line of space-separated integers.
+*/
+
+// Solution:
+
+import java.util.*;
+
+public class Solution{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int N = sc.nextInt();
+        ArrayList<Integer> L= new ArrayList<>(N);
+        
+        for(int i=0; i<N; i++)
+        {
+            L.add(sc.nextInt());
+            
+        }
+        
+        int Q= sc.nextInt();
+        for(int n=0;n<Q;n++)
+        {
+            String query = sc.next();
+            if (query.equals("Insert"))
+            {
+                int x = sc.nextInt();
+                int y = sc.nextInt();
+                L.add(x,y);
+            }
+            else if(query.equals("Delete"))
+            {
+                int x = sc.nextInt();
+                L.remove(x);
+            }
+        }
+        for(int p=0; p<L.size();p++)
+        {
+            System.out.print(L.get(p));
+            if (p!=L.size()-1)
+            {
+                System.out.print(" ");
+            }
+        }
+    }
+}
